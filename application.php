e5268c2264e9a319a18ff9bac10775e8ff62d32d
Ravi Ranjan <87768582+rranjan93340@users.noreply.github.com>
2023-01-22 19:19:47 +0530

    Add files via upload

---
+++ application.php
@@ -0,0 +1,114 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job Portal</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    
+        
+     <div class="header">
+            
+            <div class="container">
+                <h>JOB PORTAL</h>
+            </div>
+            <div class="navbar">
+                <ul>
+                    <li><a href="index.html" >Home</a></li>
+                    <li><a href="about.html" >About</a></li>
+                    <li><a href="contact.html" >Contact</a></li> 
+                </ul><hr>
+            </div> 
+        </div>
+
+    
+        <div class="section">
+            
+            <div class="text">
+            
+            <h1><font  size=45px face="Elephant">Employment <span>Application</span></font></h1>
+            <br><br>
+            <p>Please fill out the fields below as thoroughly and honestly as possible.This information will help us
+                decide if you are a good fit for any our open position<br>
+            
+           
+            <h3>Note: The section marked with a red astrisk * are no-optional.</h3>
+            
+        </div>
+        <img src="img/employee.jpg" height="350px" width="50%">
+    </p>
+    </div>
+      
+    
+    <div class="form-fill">
+        <h>WELCOME TO REGISTRATION FORM</h>
+        <P>Enter your nessessary detail and submit this form for your dream job</P>
+     
+
+        <div class="form">
+            <form  method="post">
+                <input type="text" name="name" id="name" placeholder="Enter Your Name">
+                <input type="number" name="age" id="age" placeholder="Enter Your Age">
+                <input type="text" name="gender" id="gender" placeholder="Enter Your gender">
+                <input type="Email" name="email" id="email" placeholder="Email Your Email">
+                <input type="phone" name="phone" id="phone" placeholder="Enter Your Contact Number">
+                <input type="text" name="name" id="name" placeholder="Enter your job portal name">
+
+                <textarea name="desc" name="desc" cols="30" rows="10" placeholder="Describe yourself "></textarea>
+                <button class="btns">SUBMIT</button>
+                <button class="btns">RESET</button>
+                
+                
+            </form>
+        </div>
+    </div> 
+ 
+
+
+     <!----------footer---------->
+     <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="fotter-col-1">
+                    <h3>Download this app now</h3>
+                    <p>Download app for android and ios mobile phone.</p>
+                    <div class="app-logo">
+                    <img src="img/dow.jpg" height="120px" width="280px">
+                    
+                </div>
+                </div>
+                <div class="footer-col-2">
+                    <img src="img/logo.jpg" height="80px" width="90px">
+                    <p>Our purpose is to sustainable make the pleasure and benefits of sports accessible to the many. </p>
+                </div>
+                <div class="footer-col-3">
+                    <h3>Useful Links</h3>
+                    <ul>
+                        <li>Coupons</li>
+                        <li>Bolg post</li>
+                        <li>Return policy</li>
+                        <li>Join Affiliate</li>
+                    </ul>
+                </div>
+                <div class="footer-col-4">
+                    <h3>Follow Us</h3>
+                    <ul>
+                        <li>Facebook</li>
+                        <li>Twitter</li>
+                        <li>Instagram</li>
+                        <li>Youtube</li>
+                    </ul>
+                </div>
+            </div>
+            <hr>
+            <p class="copyright">  Copyright © 2022 - RR.shopy India. All Copyright Reserved</p>
+        </div>
+
+</body>
+</html>
